6b96f5964ac77d08c51fc43dc8b8d88720e77314
Ahmed Abdulmaksoud <ahmedsalaheldin125@gmail.com>
2025-03-21 03:11:53 -0400

    refactor: moved config.yml to top level

--- src/app.py
+++ src/app.py
@@ -14,11 +14,12 @@ import utils
 def main():
     # Get config file
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    with open(os.path.join(current_dir, "config.yml"), "r") as f:
+    parent_dir = os.path.dirname(current_dir)
+
+    with open(os.path.join(parent_dir, "config.yml"), "r") as f:
         config = yml.safe_load(f)
 
     # Get the themes file and load it
-    parent_dir = os.path.dirname(current_dir)
     themes_filepath = os.path.join(parent_dir, config.get("themes_path"))
 
     themes = utils.load_raw_themes(themes_filepath)
